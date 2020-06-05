7ce498653f59688693b47188ae413c1303ac866a
Rafael Correia <rafaelcbm@gmail.com>
2020-06-05 12:25:31 -0300

    - Advanced json searches

--- src/main/java/br/com/rafaelcbm/restassured/helloworld/ResponseBodyTest.java
+++ src/main/java/br/com/rafaelcbm/restassured/helloworld/ResponseBodyTest.java
@@ -1,6 +1,10 @@
 package br.com.rafaelcbm.restassured.helloworld;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
@@ -8,12 +12,15 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
@@ -150,9 +157,51 @@ public class ResponseBodyTest {
 		// Verification
 		.then()
 			.statusCode(200)
-			//Convention $
+			// Convention $ with root
 			.body("$", hasSize(3))
+			// without $, the same
+			.body("", hasSize(3))
 			.body("age.findAll{it <= 25}.size()", is(2))
+			.body("age.findAll{it <= 25 && it>20}.size()", is(1))
+			.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+			// back to front index
+			.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+			// find first
+			.body("find{it.age <= 25}.name", is("Maria Joaquina"))
+			// contains (based on Groovy)
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+			.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+			// collect
+			.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			// both Matchers conditions - Refactored in the next test: shouldJoinJsonPathWithJava()
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"),arrayWithSize(1)))
+			// collect transformation
+			.body("age.collect{it * 2}", hasItems(60, 50, 40))
+			.body("id.max()", is(3))
+			// numbers comparison
+			.body("salary.min()", is(1234.5677f))
+			.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+			.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)))
 		;
+	}
+	
+	@Test
+	public void shouldJoinJsonPathWithJava() {
+		// Preparation
+		ArrayList<String> names = 
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			// both Matchers conditions
+			.extract().path("name.findAll{it.startsWith('Maria')}");
+		
+		Assert.assertEquals(1, names.size());
+		Assert.assertTrue(names.get(0).equalsIgnoreCase("mAria JoaquiNa"));
+		Assert.assertEquals(names.get(0).toUpperCase(), "MARIA JOAQUINA");
+		
 	}
 }
